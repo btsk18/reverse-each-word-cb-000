a17d433da64eeb38c9d02a9dbc9aecd83920be48
Brandon Leeds <brandonleeds@gmail.com>
2017-12-05 22:25:39 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,7 +9,7 @@ end
 
 def reverse_each_word(sentence)
   sentence_array = sentence.split
-  reversed_sentence = sentence_array.collect_with_index { |x, index|
+  sentence_array.collect!.with_index { |x, index|
     reversed_sentence += index == 0 ? "#{x.reverse}" : " #{x.reverse}"
   }
   return reversed_sentence
