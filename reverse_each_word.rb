501e1cb921f3d4813a60cafc11e83838a8382120
Brandon Leeds <brandonleeds@gmail.com>
2017-12-05 22:29:13 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,8 +9,6 @@ end
 
 def reverse_each_word(sentence)
   sentence_array = sentence.split
-  reversed_sentence = sentence_array.collect { |x, index|
-    x.reverse
-  }
+  sentence_array.collect! { |x, index| x.reverse }
   return reversed_sentence
 end
